0c486356ed852bfcfbb1e658a7dd1e2a9bb83d4b
João <victorpow@live.com>
2021-01-18 06:13:29 -0300

    already transferring amount across the accounts

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,2 +1,26 @@
-class AccountController < ApplicationController
+module Api
+    module V1
+        class AccountController < ApplicationController
+            def transfer
+                account = Account.find(params[:id])
+            
+                recipient_param = params.permit(:recipient_id)
+                recipient = Account.find(recipient_param[:recipient_id])
+            
+                return head :unprocessable_entity unless Account.transfer(account, recipient, amount)
+                render json: {transfered: true}
+              end
+            
+              private
+              def account_params
+                params.require(:account).permit(:name, :balance, :account_number, :token)
+              end
+            
+              def amount
+                param = params.permit(:amount)
+                param[:amount].to_i
+              end
+            end
+        end
+    end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,12 @@
 class Account < ApplicationRecord
     validates_presence_of :name, :account_number, :balance, :token
     validates_uniqueness_of :account_number, :token
+
+    def self.transfer(account, recipient, amount)
+        puts "Transfering #{amount} from account #{account.id} to account #{recipient.id}"
+        account.balance -= amount
+        recipient.balance += amount
+        recipient.save
+        account.save
+    end
 end
